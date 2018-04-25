3dd776873e39d6403ff60530c39c58da984e08a9
Anthony Lloyd <anthony@lloydhq.com>
2018-04-25 23:39:31 +0100

    message

--- MandM/Program.fs
+++ MandM/Program.fs
@@ -31,15 +31,18 @@ module LoyaltyPointsFree =
         | Pure of 'a
         | Free of Command<Program<'a>>
 
-    let rec inline private bind f = function
-        | Pure x -> f x
-        | Free (FindUser (x, next)) -> FindUser (x, next >> bind f) |> Free
-        | Free (UpdateUser (x, next)) -> UpdateUser (x, next >> bind f) |> Free
-        | Free (SendEmail (x,y,z, next)) -> SendEmail (x,y,z, next >> bind f) |> Free
-
     let inline private findUser uuid = FindUser (uuid, Pure) |> Free
     let inline private updateUser user = UpdateUser (user, Pure) |> Free
     let inline private sendEmail x y z = SendEmail (x,y,z, Pure) |> Free
+
+    let inline private map f = function
+        | FindUser (x, next) -> FindUser (x, next >> f)
+        | UpdateUser (x, next) -> UpdateUser (x, next >> f)
+        | SendEmail (x,y,z, next) -> SendEmail (x,y,z, next >> f)
+
+    let rec inline private bind f = function
+        | Pure x -> f x
+        | Free x -> map (bind f) x |> Free
 
     type private ProgramBuilder() =
         member inline __.Bind (x, f) = bind f x
@@ -49,53 +52,74 @@ module LoyaltyPointsFree =
 
     let private program = ProgramBuilder()
 
-    let addPoints userId pointsToAdd =
-        program {
-            let! u = findUser userId
-            match u with
-            | None -> return Some "User not found"
-            | Some user ->
-                let updated = { user with loyaltyPoints = user.loyaltyPoints + pointsToAdd }
-                do! updateUser updated
-                do! sendEmail user.email "Points added!" ("You now have " + string updated.loyaltyPoints)
-                return None
-        }
+    let addPoints userId pointsToAdd = program {
+        let! u = findUser userId
+        match u with
+        | None -> return Some "User not found"
+        | Some user ->
+            let updated = { user with loyaltyPoints = user.loyaltyPoints + pointsToAdd }
+            do! updateUser updated
+            do! sendEmail user.email "Points added!" ("You now have " + string updated.loyaltyPoints)
+            return None
+    }
+
+module Message =
+    open System.Threading
+
+    let wait (f : ('a -> unit) -> unit) =
+        use mre = new ManualResetEventSlim(false)
+        let mutable v = Unchecked.defaultof<_>
+        let reply a =
+            v <- a
+            mre.Set()
+        f reply
+        mre.Wait()
+        v
+
+    let waitAsync (f : ('a -> unit) -> unit) = async {
+        use mre = new ManualResetEventSlim(false)
+        let mutable v = Unchecked.defaultof<_>
+        let reply a =
+            v <- a
+            mre.Set()
+        f reply
+        let! _ = Async.AwaitWaitHandle mre.WaitHandle
+        return v
+    }
+
+
+    let postAndReply (f : ('Reply -> unit) -> 'Message) postBox =
+        wait (f >> postBox)
+
+    let postAndAsyncReply (f : ('Reply -> unit) -> 'Message) postBox =
+        waitAsync (f >> postBox)
+
+module MessageWork =
+    
+    type Message =
+        | DoSomething of int * (string -> unit)
+        | DoSomething2 of string * (int -> unit)
+
+    let postbox (_:Message) = ()
 
 module Test =
 
     open LoyaltyPointsFree
+    open MessageWork
 
-    let rec interpret = function
+
+    let run = function
+        | FindUser (x, next) -> Some {id=x; email="a@b.c"; loyaltyPoints=0} |> next
+        | UpdateUser (_, next) -> () |> next
+        | SendEmail (_,_,_, next) -> () |> next
+
+    let rec interpret p =
+        match p with
         | Pure x -> x
-        | Free (FindUser (x, next)) -> Some {id=x; email="a@b.c"; loyaltyPoints=0} |> next |> interpret
-        | Free (UpdateUser (x, next)) -> () |> next |> interpret
-        | Free (SendEmail (_,_,_, next)) -> () |> next |> interpret
+        | Free x -> run x |> interpret
 
-// case class User(id: UUID, email: String, loyaltyPoints: Int)
+    let tester = postbox |> Message.postAndReply (fun reply -> DoSomething (10, reply))
 
-// trait EmailService {
-//   def sendEmail(email: String, subject: String, body: String): Future[Unit]
-// }
-
-// trait UserRepository {
-//   def findUser(id: UUID): Future[Option[User]]
-//   def updateUser(u: User): Future[Unit]
-// }
-
-// class LoyaltyPoints(ur: UserRepository, es: EmailService) {
-//   def addPoints(userId: UUID, pointsToAdd: Int): Future[Either[String, Unit]] = {
-//     ur.findUser(userId).flatMap {
-//       case None => Future.successful(Left("User not found"))
-//       case Some(user) =>
-//         val updated = user.copy(loyaltyPoints = user.loyaltyPoints + pointsToAdd)
-//         for {
-//           _ <- ur.updateUser(updated)
-//           _ <- es.sendEmail(user.email, "Points added!", 
-//                             s"You now have ${updated.loyaltyPoints}")
-//         } yield Right(())
-//     }
-//   }
-// }
 
 [<EntryPoint>]
 let main _argv =
