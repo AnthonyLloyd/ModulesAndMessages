be02293b87dd67d427628f5f137745b115d3ace2
Anthony Lloyd <anthony@lloydhq.com>
2018-07-29 20:48:44 +0100

    remaining

--- MandM/Program.fs
+++ MandM/Program.fs
@@ -22,98 +22,108 @@ module LoyaltyPoints =
 
 module LoyaltyPointsFree =
 
-    module Outgoing =
+    type Message<'a> =
+        | FindUser of UUID * (User option->'a)
+        | UpdateUser of User * (unit->'a)
+        | SendEmail of email:string * subject:string * body:string * (unit->'a)
 
-        type Message<'a> =
-            | FindUser of UUID * (User option->'a)
-            | UpdateUser of User * (unit->'a)
-            | SendEmail of email:string * subject:string * body:string * (unit->'a)
+    type Batch<'a> =
+        | Pure of 'a
+        | Free of Message<Batch<'a>>
 
-        type Batch<'a> =
-            | Pure of 'a
-            | Free of Message<Batch<'a>>
+    let inline private findUser uuid = FindUser (uuid, Pure) |> Free
+    let inline private updateUser user = UpdateUser (user, Pure) |> Free
+    let inline private sendEmail x y z = SendEmail (x,y,z, Pure) |> Free
 
-        let inline findUser uuid = FindUser (uuid, Pure) |> Free
-        let inline updateUser user = UpdateUser (user, Pure) |> Free
-        let inline sendEmail x y z = SendEmail (x,y,z, Pure) |> Free
+    let inline private map f = function
+        | FindUser (x, next) -> FindUser (x, next >> f)
+        | UpdateUser (x, next) -> UpdateUser (x, next >> f)
+        | SendEmail (x,y,z, next) -> SendEmail (x,y,z, next >> f)
 
-        let inline map f = function
-            | FindUser (x, next) -> FindUser (x, next >> f)
-            | UpdateUser (x, next) -> UpdateUser (x, next >> f)
-            | SendEmail (x,y,z, next) -> SendEmail (x,y,z, next >> f)
+    let rec inline private bind f = function
+        | Pure x -> f x
+        | Free x -> map (bind f) x |> Free
 
-        let rec inline bind f = function
-            | Pure x -> f x
-            | Free x -> map (bind f) x |> Free
+    type BatchBuilder() =
+        member inline __.Bind (x, f) = bind f x
+        member inline __.Return x = Pure x
+        member inline __.ReturnFrom x = x
+        member inline __.Zero () = Pure ()
 
-        type BatchBuilder() =
-            member inline __.Bind (x, f) = bind f x
-            member inline __.Return x = Pure x
-            member inline __.ReturnFrom x = x
-            member inline __.Zero () = Pure ()
+    let batch = BatchBuilder()
 
-        let batch = BatchBuilder()
-
-    module Incoming =
-
-        type Message<'a> =
-            | AddPoints of UUID * int * (string option -> 'a)
-            | GetPoints of UUID * (int option -> 'a)
-
-        type Batch<'a> =
-            | Pure of 'a
-            | Free of Message<Batch<'a>>
-
-        let inline addPoints u i = AddPoints (u,i, Pure) |> Free
-        let inline getPoints u = GetPoints (u, Pure) |> Free
-
-        let inline map f = function
-            | AddPoints (u,i, next) -> AddPoints (u,i, next >> f)
-            | GetPoints (u, next) -> GetPoints (u, next >> f)
-
-        let rec inline bind f = function
-            | Pure x -> f x
-            | Free x -> map (bind f) x |> Free
-
-        type BatchBuilder() =
-            member inline __.Bind (x, f) = bind f x
-            member inline __.Return x = Pure x
-            member inline __.ReturnFrom x = x
-            member inline __.Zero () = Pure ()
-
-        let private batch = BatchBuilder()
-
-
-    let addPoints userId pointsToAdd = Outgoing.batch {
-        let! u = Outgoing.findUser userId
+    let addPoints userId pointsToAdd = batch {
+        let! u = findUser userId
         match u with
         | None -> return Some "User not found"
         | Some user ->
             let updated = { user with loyaltyPoints = user.loyaltyPoints + pointsToAdd }
-            do! Outgoing.updateUser updated
-            do! Outgoing.sendEmail user.email "Points added!" ("You now have " + string updated.loyaltyPoints)
+            do! updateUser updated
+            do! sendEmail user.email "Points added!" ("You now have " + string updated.loyaltyPoints)
             return None
     }
 
-    let getPoints userId = Outgoing.batch {
-        let! u = Outgoing.findUser userId
+    let getPoints userId = batch {
+        let! u = findUser userId
         return u |> Option.map (fun u -> u.loyaltyPoints)
     }
 
-    let outgoingInterpreter (b:Outgoing.Batch<'a>) : 'a =
+    type Interpreter =
+        abstract member Run : Batch<'a> -> 'a
+
+module EmailerModule =
+
+    type Batch<'a> =
+        | X
+
+    let sendEmail (email:string) (subject:string) (body:string) = Batch<unit>.X
+
+    let sendEmailAsync (email:string) (subject:string) (body:string) = async {
+        return Batch<unit>.X
+    }
+
+module EmailerInterpreter =
+
+    open EmailerModule
+    let rec interpret (p:Batch<'a>) : 'a =
         failwith "hi"
 
-    let run = function
-        | Incoming.AddPoints (u,i, next) -> addPoints u i |> outgoingInterpreter |> next
-        | Incoming.GetPoints (u, next) -> getPoints u |> outgoingInterpreter |> next
+    let rec interpretAsync (p:Batch<'a>) : Async<'a> =
+        failwith "hi"
 
-    let rec private incomingInterpret (batch:Incoming.Batch<'b>) =
-        match batch with
-        | Incoming.Pure x -> x
-        | Incoming.Free x -> run x |> incomingInterpret
+module Test =
 
-    let post (batch:Incoming.Batch<'b>) =
-        incomingInterpret batch
+    open LoyaltyPointsFree
+
+    let rec interpret p =
+        let run x =
+            match x with
+            | FindUser (x, next) -> Some {id=x; email="a@b.c"; loyaltyPoints=0} |> next
+            | UpdateUser (_, next) -> () |> next
+            | SendEmail (email,subject,body, next) ->
+                EmailerModule.sendEmail email subject body
+                |> EmailerInterpreter.interpret
+                |> next
+        match p with
+        | Pure x -> x
+        | Free x -> run x |> interpret
+
+    let rec interpretAsync p = async {
+        let inline run x = async {
+            match x with
+            | FindUser (x, next) -> return Some {id=x; email="a@b.c"; loyaltyPoints=0} |> next
+            | UpdateUser (_, next) -> return next()
+            | SendEmail (email,subject,body, next) ->
+                let! message = EmailerModule.sendEmailAsync email subject body
+                do! EmailerInterpreter.interpretAsync message
+                return next()
+        }
+        match p with
+        | Pure x -> return x
+        | Free x ->
+            let! result = run x
+            return! interpretAsync result
+    }
 
 
 // module Message =
@@ -145,23 +155,6 @@ module LoyaltyPointsFree =
 
 //     let postAndAsyncReply (f : ('Reply -> unit) -> 'Message) postBox =
 //         waitAsync (f >> postBox)
-
-
-// module Test =
-
-//     open LoyaltyPointsFree
-//     open MessageWork
-
-
-//     let run = function
-//         | FindUser (x, next) -> Some {id=x; email="a@b.c"; loyaltyPoints=0} |> next
-//         | UpdateUser (_, next) -> () |> next
-//         | SendEmail (_,_,_, next) -> () |> next
-
-//     let rec interpret p =
-//         match p with
-//         | Pure x -> x
-//         | Free x -> run x |> interpret
 
 //     let tester = postbox |> Message.postAndReply (fun reply -> DoSomething (10, reply))
 
